a09aee5dd21f64e040e448e6533ce1258dea04b4
Aaron Marburg <amarburg@apl.washington.edu>
2017-01-24 09:49:35 -0800

    Cleaning up the library list.

--- conanfile.py
+++ conanfile.py
@@ -17,11 +17,11 @@ class G2OConan(ConanFile):
 
   def build(self):
     cmake = CMake(self.settings)
-    cmake_opts = ""
+    cmake_opts = "-DG2O_USE_OPENGL=False -DG2O_BUILD_APPS=False -DG2O_BUILD_EXAMPLES=False"
     build_opts = ""
 
     if self.options.shared:
-      cmake_opts = "-DBUILD_SHARED_LIBS=True"
+      cmake_opts += " -DBUILD_SHARED_LIBS=True"
 
     if self.options.build_parallel:
       build_opts = "-- -j"
@@ -36,33 +36,26 @@ class G2OConan(ConanFile):
     self.copy("config.h", src="g2o", dst="include/g2o")
     if self.options.shared:
       if self.settings.os == "Macos":
-          self.copy(pattern="*.dylib", dst="lib", keep_path=False)
+          self.copy(pattern="*.dylib", dst="lib", src="g2o/lib", keep_path=False)
       else:
-          self.copy(pattern="*.so*", dst="lib", src="lib", keep_path=False)
+          self.copy(pattern="*.so*", dst="lib", src="g2o/lib", keep_path=False)
     else:
         self.copy(pattern="*.a", dst="lib", src="lib", keep_path=False)
 
   def package_info(self):
     ## I'm sure there's a much more compact python-ism for this (I could write the one-liner in Ruby...)
+    libs = ["g2o_solver_eigen", \
+                  "g2o_solver_pcg", "g2o_core", "g2o_solver_slam2d_linear",\
+                  "g2o_csparse_extension", "g2o_solver_structure_only",\
+                   "g2o_stuff", "g2o_types_data",\
+                     "g2o_types_icp", "g2o_types_sba",\
+                      "g2o_types_sclam2d", "g2o_types_sim3",\
+                        "g2o_types_slam2d_addons", "g2o_solver_cholmod",\
+                        "g2o_types_slam2d", "g2o_solver_csparse", "g2o_types_slam3d_addons",\
+                         "g2o_solver_dense", "g2o_types_slam3d"]
+
+
     if self.settings.build_type == 'Debug':
-     self.cpp_info.libs = ["g2o_calibration_odom_laser_d", "g2o_solver_eigen_d", "g2o_cli_d" ,\
-                  "g2o_solver_pcg_d", "g2o_core_d", "g2o_solver_slam2d_linear_d",\
-                  "g2o_csparse_extension_d", "g2o_solver_structure_only_d",\
-                   "g2o_ext_freeglut_minimal_d", "g2o_stuff_d", "g2o_hierarchical_d",\
-                    "g2o_tutorial_slam2d_d", "g2o_incremental_d", "g2o_types_data_d",\
-                     "g2o_interactive_d", "g2o_types_icp_d", "g2o_interface_d", "g2o_types_sba_d",\
-                      "g2o_opengl_helper_d", "g2o_types_sclam2d_d", "g2o_parser_d", "g2o_types_sim3_d",\
-                       "g2o_simulator_d", "g2o_types_slam2d_addons_d", "g2o_solver_cholmod_d",\
-                        "g2o_types_slam2d_d", "g2o_solver_csparse_d", "g2o_types_slam3d_addons_d",\
-                         "g2o_solver_dense_d", "g2o_types_slam3d_d"]
+     self.cpp_info.libs = [ name+"_d" for name in libs ]
     else:
-      self.cpp_info.libs = ["g2o_calibration_odom_laser", "g2o_solver_eigen", "g2o_cli" ,\
-                      "g2o_solver_pcg", "g2o_core", "g2o_solver_slam2d_linear",\
-                      "g2o_csparse_extension", "g2o_solver_structure_only",\
-                       "g2o_ext_freeglut_minimal", "g2o_stuff", "g2o_hierarchical",\
-                        "g2o_tutorial_slam2d", "g2o_incremental", "g2o_types_data",\
-                         "g2o_interactive", "g2o_types_icp", "g2o_interface", "g2o_types_sba",\
-                          "g2o_opengl_helper", "g2o_types_sclam2d", "g2o_parser", "g2o_types_sim3",\
-                           "g2o_simulator", "g2o_types_slam2d_addons", "g2o_solver_cholmod",\
-                            "g2o_types_slam2d", "g2o_solver_csparse", "g2o_types_slam3d_addons",\
-                             "g2o_solver_dense", "g2o_types_slam3d"]
+      self.cpp_info.libs = libs
